c6dc509f814a44324a362e5c60619e3a16cd5447
ananaziz96 <ananaziz15@gmail.com>
2019-12-29 12:24:02 -0700

    ready to merge

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,4 @@
 let myApp = angular.module("myNinjaApp", ["ngRoute"]);
-
 //The function passed in can be fired when before our app runs/loads, etc.
 //i.e. routing
 myApp.config([
@@ -18,7 +17,6 @@ myApp.config([
       });
   }
 ]);
-
 //The function passed in can be fired when our app runs/loads, etc.
 myApp.run(function() {});
 
@@ -33,7 +31,6 @@ myApp.controller("NinjaController", [
       var index = $scope.ninjas.indexOf(item);
       $scope.ninjas.splice(index, 1);
     };
-
     //Adding a new ninja
     $scope.addNinja = function() {
       $scope.ninjas.push({
@@ -42,52 +39,44 @@ myApp.controller("NinjaController", [
         rate: $scope.newNinja.rate,
         available: true
       });
-
       //Clear the form after adding
       $scope.newNinja.name = "";
       $scope.newNinja.belt = "";
       $scope.newNinja.rate = "";
     };
 
-    $http.get("data/ninja-list.json").success(function(data) {
-      $scope.ninjas = data;
-    });
+    $scope.ninjas = [
+      {
+        name: "Yoshi",
+        belt: "orange",
+        rate: 50,
+        available: true,
+        imageSource: "images/image1.jpg"
+      },
+      {
+        name: "Crystal",
+        belt: "green",
+        rate: 100,
+        available: true,
+        imageSource: "images/image2.jpg"
+      },
+      {
+        name: "Kenji",
+        belt: "brown",
+        rate: 150,
+        available: false,
+        imageSource: "images/image3.jpg"
+      },
+      {
+        name: "Shaun",
+        belt: "blue",
+        rate: 200,
+        available: true,
+        imageSource: "images/image4.jpg"
+      }
+    ];
 
-    console.log($http.get("data/ninja-list.json"));
-
-    //This data is in data/ninja-list.json
-    // $scope.ninjas = [
-    //   {
-    //     name: "Yoshi",
-    //     belt: "orange",
-    //     rate: 50,
-    //     available: true,
-    //     imageSource: "images/image1.jpg"
-    //   },
-    //   {
-    //     name: "Crystal",
-    //     belt: "green",
-    //     rate: 100,
-    //     available: true,
-    //     imageSource: "images/image2.jpg"
-    //   },
-    //   {
-    //     name: "Kenji",
-    //     belt: "brown",
-    //     rate: 150,
-    //     available: false,
-    //     imageSource: "images/image3.jpg"
-    //   },
-    //   {
-    //     name: "Shaun",
-    //     belt: "blue",
-    //     rate: 200,
-    //     available: true,
-    //     imageSource: "images/image4.jpg"
-    //   }
-    // ];
-
-    //Converts the ninjas ob
+    //Converts a object to json
     // console.log(angular.toJson($scope.ninjas));
   }
 ]);
